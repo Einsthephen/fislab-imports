ecd78c57e0a57afa252dc30924caa17c9e520132
Juan <101371618+ProfRTRD@users.noreply.github.com>
2022-04-06 00:29:06 -0500

    Add files via upload

--- HW2.cpp
+++ HW2.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-#include <fstream> // escribir o leer archivos
+#include <fstream>
 #include <cmath>
 double sph_value(unsigned n, double x);
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
   std::ofstream fout{"data.txt"};
-   for(double x = 0;  x <= 10; x= x+0.05) {
-
-     fout << x <<"\t"<< sph_value(0, x)<<"   "<< sph_value(1, x) << '\n';
-
-    
-  }
+  double dx {0.05};
+  int num_steps = 10/dx;
+  int num=0;
+   for(int i = 0;  i <=num_steps ; i++) {
+     double x = i*dx;
+     if (num==0){
+     fout << x <<"\t "<< sph_value(0, x)<<"\t \t "<< sph_value(1, x) << '\n';
+     num= 1;
+     }
+     else {fout << x <<"\t "<< sph_value(0, x)<<"\t "<< sph_value(1, x) << '\n';}
+   }
    fout.close();
     return 0;  
 }
 
 double sph_value(unsigned n = 0, double x = 0){
-  //aqui piensas en que va a desarrollar la funcion
   double value = std::sph_bessel(n , x);
-
   return value;
 }
-
