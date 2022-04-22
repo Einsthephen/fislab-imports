046e5c02d58e343ec12e4739f9e22d42429723c0
Einsthephen <79174656+Einsthephen@users.noreply.github.com>
2022-04-21 22:33:37 -0500

    Update Richardson.cpp

--- Richardson.cpp
+++ Richardson.cpp
@@ -11,58 +11,59 @@ double richardson_derivFWD(double x, double h);
 int main(void)
 {
   std::cout.precision(15); std::cout.setf(std::ios::scientific);
-  double h, x, df, dc, drc, drf, dexact;
-
-  x = 3.7;
+  double x = 3.7;
+  double dexact = 2*std::cos(2*x);
   for (int ii = 1; ii <= 10; ii++) {
-    h = std::pow(10.0, -ii);
-    df = forward_deriv(x, h);
-    dc = central_deriv(x, h);
-    drc = richardson_derivcentral(x, h);
-    drf = richardson_derivFWD(x, h);
-    dexact = 2*std::cos(2*x);
+    double h = std::pow(10.0, -ii);
+    double df = forward_deriv(x, h);
+    double dc = central_deriv(x, h);
+    double drc = richardson_derivcentral(x, h);
+    double drf = richardson_derivFWD(x, h);
+    
     std::cout << h << "\t"
-              << std::fabs(df-dexact)/dexact << "\t"
-              << std::fabs(dc-dexact)/dexact << "\t"
-	      << std::fabs(drf-dexact)/dexact << "\t"
-              << std::fabs(drc-dexact)/dexact << "\n";
-
-    // std::printf("%25.16e%25.16e%25.16e%25.16e\n", h,
-    //             std::fabs(df-dexact)/dexact,
-    //             std::fabs(dc-dexact)/dexact,
-    //             std::fabs(dr-dexact)/dexact);
+              << fabs(df-dexact)/dexact << "\t"
+              << fabs(dc-dexact)/dexact << "\t"
+	      << fabs(drf-dexact)/dexact << "\t"
+              << fabs(drc-dexact)/dexact << "\n";
   }
 
   return 0;
 }
 
 double f(double x)
-{
-  return std::sin(2*x);
+{ 
+  double resp =sin(2*x);
+  return resp;
 }
 
 double forward_deriv(double x, double h)
-{
-  return (f(x+h) - f(x))/h;
+{ 
+  double a= f(x+h);
+  double b = f(x);
+  double resp = (a - b)/h;
+  return resp;
 }
 
 double central_deriv(double x, double h)
 {
-  return (f(x + h/2) - f(x - h/2))/h;
+  double a= f(x + h/2);
+  double b = f(x - h/2);
+  double resp = (a - b)/h;
+  return resp;
 }
 
 double richardson_derivcentral(double x, double h)
 {
-  double c1, c2;
-  c1 = central_deriv(x, h);
-  c2 = central_deriv(x, h/2);
-  return (4*c2 - c1)/3.0;
+  double c1 = central_deriv(x, h);
+  double c2 = central_deriv(x, h/2);
+  double resp= (4*c2 - c1)/3.0;
+  return resp;
 }
 
 double richardson_derivFWD(double x, double h)
-{
-  double f1, f2;
-  f1 = forward_deriv(x, h);
-  f2 = forward_deriv(x, h/2);
-  return (2*f2 - f1);
+{ 
+  double f1 = forward_deriv(x, h);
+  double f2 = forward_deriv(x, h/2);
+  double resp = (2*f2 - f1);
+  return resp;
 }
